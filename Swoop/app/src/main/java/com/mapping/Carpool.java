4a98235374b68d62bfe25e6b94e8eb4b0d8f33ee
Yarely Chino <yarely2011@yahoo.com>
2016-10-05 22:58:44 -0700

    Created Tables: Carpool, User, Notification, Review, and Vehicle using Amazon Dynamodb. Initilized the AWS Cognito credentials, DynamoDB client, and implemented the Carpool mapper class

---
+++ Swoop/app/src/main/java/com/mapping/Carpool.java
@@ -0,0 +1,57 @@
+package com.mapping;
+
+/**
+ * Created by ychino on 10/4/16.
+ */
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+
+@DynamoDBTable(tableName = "Carpool")
+public class Carpool {
+
+    //TODO: create startLocation and endLocation instance variables
+    private String carpoolId;
+    private String userId;
+    private String timeStamp;
+    private double rate;
+    private int numberOfPassengers;
+    private String carpoolStatus;
+    private Boolean isCompleted;
+    private Boolean isDeleted;
+
+    @DynamoDBHashKey(attributeName="carpoolId")
+    public String getId() { return carpoolId;}
+    public void setId(String carpoolId) {this.carpoolId = carpoolId;}
+
+    @DynamoDBAttribute(attributeName="userId")
+    public String getUserId() {return userId;}
+    public void setUserId(String userId) {this.userId = userId;}
+
+    @DynamoDBAttribute(attributeName="timeStamp")
+    public String getTimeStamp() {return timeStamp;}
+    public void setTimeStamp(String timeStamp) {this.timeStamp = timeStamp;}
+
+    @DynamoDBAttribute(attributeName="rate")
+    public double getRate() {return rate;}
+    public void setRate(double rate) {this.rate = rate;}
+
+    @DynamoDBAttribute(attributeName="numberOfPassengers")
+    public int getNumberOfPassengers() {return numberOfPassengers;}
+    public void setNumberOfPassengers(int numberOfPassengers) {this.numberOfPassengers = numberOfPassengers;}
+
+    @DynamoDBAttribute(attributeName="carpoolStatus")
+    public String getCarpoolStatus() {return carpoolStatus;}
+    public void setCarpoolStatus(String carpoolStatus) {this.carpoolStatus = carpoolStatus;}
+
+    @DynamoDBAttribute(attributeName="isCompleted")
+    public Boolean getIsCompleted() {return isCompleted;}
+    public void setIsCompleted(Boolean completed) {isCompleted = completed;}
+
+    @DynamoDBAttribute(attributeName="isDeleted")
+    public Boolean getIsDeleted() {return isDeleted;}
+    public void setIsDeleted(Boolean deleted) {isDeleted = deleted;}
+
+
+
+
+}
--- Swoop/app/src/main/java/com/swoop/swoop/MainActivity.java
+++ Swoop/app/src/main/java/com/swoop/swoop/MainActivity.java
@@ -1,13 +1,81 @@
 package com.swoop.swoop;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.mapping.Carpool;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button databaseButon;
+    private CognitoCachingCredentialsProvider credentialsProvider;
+    private AmazonDynamoDBClient ddbClient;
+    private DynamoDBMapper mapper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the Amazon AWS Cognito credentials provider
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-west-2:16a412e8-b5eb-49f0-9bfc-78c6c5ecad1c", // Identity Pool ID
+                Regions.US_WEST_2 // Region
+        );
+
+
+        //Instantiate a DynamoDB client using the credentialsProvider
+        ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+
+        ddbClient.setRegion(Region.getRegion(Regions.US_WEST_2));  // Add correct Region. In my case its EU_WEST_1
+
+        //DynamoDB Object Mapper to map a client-side class to our database
+        mapper = new DynamoDBMapper(ddbClient);
+
+
+        databaseButon = (Button) findViewById(R.id.databaseButton);
+        databaseButon.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                databaseConnenction(v);
+            }
+        });
+    }
+
+    public void databaseConnenction(View v){
+
+        Context context = v.getContext();
+        Toast.makeText(context, "Button was clicked", Toast.LENGTH_SHORT).show();
+
+
+        final Runnable runnable = new Runnable() {
+            public void run() {
+                //DynamoDB calls go here
+                Log.d("MainActivity", "Inside new Runnable Thread");
+
+
+                Carpool carRetrieved = mapper.load(Carpool.class, "123A");//retreived car with id 123A
+                Log.d("MainActivity", "ID: " + carRetrieved.getId() + " " + carRetrieved.getRate());
+
+            }
+        };
+        Thread mythread = new Thread(runnable);
+        mythread.start();
+
     }
 }
