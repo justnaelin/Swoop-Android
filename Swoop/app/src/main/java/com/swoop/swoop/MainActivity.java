4effc83e0f15433888ffba679b56d16b102acccd
Yarely Chino <yarely2011@yahoo.com>
2016-11-14 22:14:39 -0800

    Refactored MainActivity, using code smell Long Method
    Summary: This refactors the onCreate method and breaks it down to smaller methods that initialize the drawer and first fragment.
    
    Test Plan: Test have successfully passed locally.
    
    Reviewers: kp1zano
    
    Subscribers:

--- Swoop/app/src/main/java/com/swoop/swoop/MainActivity.java
+++ Swoop/app/src/main/java/com/swoop/swoop/MainActivity.java
@@ -33,21 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //All Drawer Navigation Items initialized
-        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_0), getString(R.string.drawer_description_0), R.mipmap.ic_launcher));
-        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_1), getString(R.string.drawer_description_1), R.mipmap.ic_launcher));
-        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_2), getString(R.string.drawer_description_2), R.mipmap.ic_launcher));
-        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_3), getString(R.string.drawer_description_3), R.mipmap.ic_launcher));
-        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_4), getString(R.string.drawer_description_4), R.mipmap.ic_launcher));
-
-
-        // Initializing DrawerLayout
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-
-        // Populate the Navigation Drawer with options
-        mDrawerPanel = (RelativeLayout) findViewById(R.id.drawerPane);
-        mDrawerList = (ListView) findViewById(R.id.navList);
+        //initialize main drawer content
+        initDrawer();
 
         //Adapter for the Drawer List
         DrawerListAdapter adapter = new DrawerListAdapter(this, mDrawerItems);
@@ -60,6 +47,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 itemSelectedFromDrawer(position);
             }
         });
+
+        //initialize the first fragment display
+        initFirstFragment();
+
+    }
+
+    private void initDrawer(){
+
+        //All Drawer Navigation Items initialized
+        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_0), getString(R.string.drawer_description_0), R.mipmap.ic_launcher));
+        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_1), getString(R.string.drawer_description_1), R.mipmap.ic_launcher));
+        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_2), getString(R.string.drawer_description_2), R.mipmap.ic_launcher));
+        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_3), getString(R.string.drawer_description_3), R.mipmap.ic_launcher));
+        mDrawerItems.add(new DrawerItem(getString(R.string.drawer_title_4), getString(R.string.drawer_description_4), R.mipmap.ic_launcher));
+
+        // Initializing DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Populate the Navigation Drawer with options
+        mDrawerPanel = (RelativeLayout) findViewById(R.id.drawerPane);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+    }
+
+    private void initFirstFragment(){
 
         fragment = new HomeFragment();
         getFragmentManager().beginTransaction()
@@ -193,5 +204,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return view;
         }
     }
+
+
 
 }
