34028937cb73421167dd7b26d1f03972a5003bed
Yarely Chino <yarely2011@yahoo.com>
2016-11-14 21:49:55 -0800

    Added tests for CreateCarpoolActivity and MainActivity
    Summary: The tests test mainDrawer and it's content, along with the crateCarpool activity.
    
    Test Plan: This tests our MainActivity and CreateCarpool
    
    Reviewers: kp1zano
    
    Subscribers:

--- Swoop/app/src/androidTest/java/com/swoop/swoop/MainActivityTest.java
+++ Swoop/app/src/androidTest/java/com/swoop/swoop/MainActivityTest.java
@@ -1,9 +1,27 @@
 package com.swoop.swoop;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.closeDrawer;
+import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
+import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * MainActivityTest
@@ -19,7 +37,52 @@ public class MainActivityTest {
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void shouldShowSwoopFragmentName() {
+    public void testOpenAndCloseDrawer() {
+
+        openDrawer(R.id.drawerLayout);
+        closeDrawer(R.id.drawerLayout);
+
+        openDrawer(R.id.drawerLayout);
+        onView(withId(R.id.drawerLayout)).check(matches(isOpen()));
 
     }
+
+    @Test
+    public void testOpenDrawerAndItemTitle() {
+
+        openDrawer(R.id.drawerLayout);
+        onData(anything())
+                .inAdapterView(withId(R.id.navList))
+                .atPosition(0)
+                .onChildView(withId(R.id.title))
+                .check(matches(isDisplayed()));
+
+        //check if the title content is being display properly
+        onView(withId(R.string.drawer_title_0));
+        onView(withId(R.string.drawer_title_1));
+        onView(withId(R.string.drawer_title_2));
+        onView(withId(R.string.drawer_title_3));
+
+    }
+
+    @Test
+    public void testOpenDrawerAndItemDescription() {
+        openDrawer(R.id.drawerLayout);
+
+        onData(anything())
+                .inAdapterView(withId(R.id.navList))
+                .atPosition(0)
+                .onChildView(withId(R.id.title))
+                .check(matches(isDisplayed()));
+
+        //check if the description content is being display properly
+        onView(withId(R.string.drawer_description_0));
+        onView(withId(R.string.drawer_description_1));
+        onView(withId(R.string.drawer_description_2));
+        onView(withId(R.string.drawer_description_3));
+
+
+    }
+
+
 }
