d84382132fc8e03ce5b83ec5b461a8fc5cb253c1
kp1zano <kpizano@csumb.edu>
2016-11-24 11:01:39 -0800

    Latitude and Longitude is the new Address. Delted Address file that will no longer be needed, updated Gradle to include Mockito Tests
    Summary: Latitide Longitude is the new Address (Google Places API). Deleted Address file. Updated Gradle to include Mockito Tests.
    
    Test Plan: Create Mockito Tests
    
    Reviewers: yarelyc
    
    Subscribers:

--- Swoop/app/src/test/java/com/swoop/swoop/ExampleUnitTest.java
+++ Swoop/app/src/test/java/com/swoop/swoop/ExampleUnitTest.java
@@ -1,15 +1,31 @@
 package com.swoop.swoop;
 
+import com.rest.CarpoolResource;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mock;
+
+
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testCreateCarpoolRequest(){
+
+        CarpoolResource cResource = Mockito.mock(CarpoolResource.class);
+
     }
 }
