bc320e7510f6d5c1e3e873229d3734797eca7ec1
ana <anaperez@csumb.edu>
2016-11-26 16:02:46 -0800

    merge conflicts

--- Swoop/app/src/main/java/com/mapping/User.java
+++ Swoop/app/src/main/java/com/mapping/User.java
@@ -1,25 +1,44 @@
 package com.mapping;
 
+import java.util.List;
+
 /**
  * Created by ana perez on 10/6/16.
  * Summary: User Mapper
  */
 
-
 public class User {
     private String userId;
-    private String name;
+    private String firstName;
     private String lastName;
+    private String address;
     private String emailAddress;
     private double averageRating;
     private String phoneNumber;
+    private List<String> requestedCarpoolIds;
+    private List<String>  reviewIds;
+    private String vehicleId;
     private String birthday;
+    public User(String userId, String firstName, String lastName, String address, String emailAddress,
+                Double averageRating, String phoneNumber, String birthday, List<String> requestedCarpoolIds, String vehicleId, List<String> reviewIds) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.averageRating = averageRating;
+        this.birthday = birthday;
+        this.requestedCarpoolIds = requestedCarpoolIds;
+        this.vehicleId = vehicleId;
+        this.reviewIds = reviewIds;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+    }
 
     public void setUserId(String userId) {this.userId = userId;}
     public String getUserId() {return userId;}
 
-    public void setName(String name) {this.name = name;}
-    public String getName() {return name;}
+    public void setName(String name) {this.firstName = name;}
+    public String getName() {return firstName;}
 
     public void setLastName(String lastName) {this.lastName = lastName;}
     public String getLastName() {return lastName; }
@@ -39,10 +58,38 @@ public class User {
     @Override
     public String toString() {
         return  "userID: " + userId + "\n"
-                + "name: " + name + String.format("\nAverageRating: %s", averageRating) + "\n"
+                + "name: " + firstName + String.format("\nAverageRating: %s", averageRating) + "\n"
                 + "lastName: " + lastName + "\n"
                 + "phoneNumber: "  + phoneNumber + "\n"
                 + "emailAddress: " + emailAddress + "\n"
+                + "requestedCarpoolIDS: " + requestedCarpoolIds + "\n"
+                + "reviewIds: "  + reviewIds + "\n"
+                + "vehicleID: " + vehicleId + "\n"
+                + "address: "  + address + "\n"
                 + "birthday: " + birthday +"\n";
+    }
+
+    public List<String> getRequestedCarpoolIds() {
+        return requestedCarpoolIds;
+    }
+
+    public void setRequestedCarpoolIds(List<String> requestedCarpoolIds) {
+        this.requestedCarpoolIds = requestedCarpoolIds;
+    }
+
+    public List<String> getReviewIds() {
+        return reviewIds;
+    }
+
+    public void setReviewIds(List<String> reviewIds) {
+        this.reviewIds = reviewIds;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
     }
 }
